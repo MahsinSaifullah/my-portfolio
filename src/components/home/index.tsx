9f41c3e35d04ad6a1401c6b5e6ccea5e83c33309
macbookpro <mahsinsaifullah@gmail.com>
2021-01-06 11:11:38 +0000

    added an arrow icon to home page view my work button

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { InlineIcon } from '@iconify/react';
+import bxRightArrowAlt from '@iconify-icons/bx/bx-right-arrow-alt';
+import bxDownArrowAlt from '@iconify-icons/bx/bx-down-arrow-alt';
+
 import './index.scss';
 
 const Home: React.FC = () => {
+  const [isMouseOver, setIsMouseOver] = useState(false);
   return (
     <div className="home">
       <div className="home__title">
         Hello I'm <span>Mahsin Saifullah</span>
         <br />I am a software developer
       </div>
-      <div className="home__button">View my Work</div>
+      <div
+        onMouseOver={() => setIsMouseOver(true)}
+        onMouseLeave={() => setIsMouseOver(false)}
+        className="home__button"
+      >
+        View my Work{' '}
+        {isMouseOver ? (
+          <InlineIcon className="home__icon" icon={bxDownArrowAlt} />
+        ) : (
+          <InlineIcon className="home__icon" icon={bxRightArrowAlt} />
+        )}
+      </div>
     </div>
   );
 };
